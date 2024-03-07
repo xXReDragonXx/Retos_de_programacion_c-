2cde6778fedadbebf6f5e6d13a54b55835d3d16a
Cervantes <devscervantes@gmail.com>
2024-03-07 13:53:20 -0600

    El famoso Fizz Buzz

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* RETO EL FAMOSO FIZZ BUZZ
+            * Escribe un programa que muestre por consola (con un print) los
+            * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+            * cada impresión), sustituyendo los siguientes:
+            * - Múltiplos de 3 por la palabra "fizz".
+            * - Múltiplos de 5 por la palabra "buzz".
+            * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+*/
+            Reto FizzBuzz = new Reto();
+            FizzBuzz.Resolverfizzconfor(20);
+            Reto FizzBuzzA = new Reto();
+            FizzBuzzA.Resolverfizzconforeach(20);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasCSharp
+{
+    public class Reto
+    {
+        public void Resolverfizzconfor(int limite)
+        {
+            for (int i = 1; i <= limite; i++)
+            {
+                int multiplodetres = i % 3;
+                int multiplodecinco = i % 5;
+
+                if (multiplodetres == 0 && multiplodecinco == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (multiplodetres == 0)
+                    Console.WriteLine("Fizz");
+                else if (multiplodecinco == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+        }
+
+        public void Resolverfizzconforeach(int limite)
+        {
+            List<int> Arraydel1al100 = new List<int>();
+            for (int i = 1; i <= limite; i++)
+            {
+                Arraydel1al100.Add(i);
+            }
+
+            foreach (int numero in Arraydel1al100)
+            {
+                int multiplodetres = numero % 3;
+                int multiplodecinco = numero % 5;
+
+                if (multiplodetres == 0 && multiplodecinco == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (multiplodetres == 0)
+                    Console.WriteLine("Fizz");
+                else if (multiplodecinco == 0)
+                    Console.WriteLine("Buzz");
+                else Console.WriteLine(numero);
+            }
+        }
+    }
+}
